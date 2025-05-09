0875caa572cabb7af742da94d948e89c38f846ac
Harry Waterman <viceversa1024@gmail.com>
2025-05-09 14:40:57 -0700

    Deploy from CLI works.

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,11 @@ cd build
 pip install -r ../requirements.lambda.txt -t .
 cp ../lambda_function.py .
 cp -r ../html .
-ZIPFILE=barcode-responder-lambda.$(date +%Y-%m-%d-%H-%M).zip
+SHA=$(git log --oneline | cut -b1-7 | head -1)
+ZIPFILE=barcode-responder-lambda.$(date +%Y-%m-%d-%H-%M).$SHA.zip
 ARCHIVE=$HOME/$ZIPFILE
 zip -r $ARCHIVE .
-aws s3 cp $ARCHIVE s3://thehirschhorn.com.lambda.sources/
+aws --profile keene s3 cp $ARCHIVE s3://thehirschhorn.com.lambda.sources/
 aws --profile keene lambda update-function-code \
     --function-name barcodeResponder312 \
     --s3-key $ZIPFILE \
